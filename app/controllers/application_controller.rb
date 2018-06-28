29e7bb0a76f06fa0c0319a8bb3b2464403f85328
Shall16 <shall130@rsdmo.org>
2018-06-28 23:38:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
+
+
+require './app/models/model'
+
 class ApplicationController < Sinatra::Base
-
-  configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
-  end
-
-  get "/" do
-  	erb :index
-  end
-end
+  configure do 
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end 
+  get '/' do 
+    erb :index
+  end 
+  post '/' do #post= we've clicked the button now what?
+    @the_fortune=get_fortune #Method get_fortune is csonncecet to the instance variable @the_fortune 
+    @the_user=params[:user] #creating a new instance variable the_user params= to a hash that stores user input
+    return erb :results
+  end 
+end 
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,12 @@
-class Model
-  # Replace with Dog clas
-end
+def get_fortune 
+  
+  fortune_array=["You will be succseful on your group project","You will enjoy life", "You will lose your left shoe this afternoon","you will eat bok choy in your near future","You will get a nose bleed on the metro","YOu will coem across a field of bok choy"]
+  return fortune_array.sample 
+  # fortune_array.delate(fortune)
+  # return fortune 
+end 
+
+#puts get_fortune
+  
+  
+  
